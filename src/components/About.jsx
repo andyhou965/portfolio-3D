0ff886f1378f08519565f0bab830a785a0bf2464
Junjie Hou <andyhou965@gmail.com>
2023-10-21 19:37:17 +0400

    Update the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview.</h2>
+			</motion.div>
+			<motion.p variants={fadeIn("", "", 0.1, 1)}></motion.p>
+		</>
+	);
+};
 
-export default About
+export default About;
